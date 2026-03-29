62f8117abcd067b3246ef62de22511d06cbeb0ca
rob587 <robertocamm02@yahoo.com>
2026-03-29 12:45:05 +0200

    Setting Layout for navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,25 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
+        options={{ title: "Home", tabBarIcon: () => <Text>🏠</Text> }}
+      />
+      <Tabs.Screen
+        name="esplora"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Esplora",
+          tabBarIcon: () => <Text>🔍</Text>,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="profilo"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Profilo",
+          tabBarIcon: () => <Text>👤</Text>,
         }}
       />
     </Tabs>
